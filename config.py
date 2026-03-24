8288d47de52818fff02a61c40f67c4bd58d720bb
melih022 <110200865+melih022@users.noreply.github.com>
2026-03-24 23:02:16 +0300

    Set default UPSTREAM_REPO for Git configuration

--- config.py
+++ config.py
@@ -44,7 +44,7 @@ HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 # ── Git / updates ──────────────────────────────────────────────────────────────
-UPSTREAM_REPO = getenv("UPSTREAM_REPO", None)
+UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/melih022/g-ncel")
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv("GIT_TOKEN")  # needed if repo is private
 
